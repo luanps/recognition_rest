0ecbbbfae357a3b7c770290c6365a5f26c7701ac
luanps <luann.porfirio@gmail.com>
2019-06-23 18:13:17 -0300

    add functions to convert image into base64 and vice-versa. fix identation

--- recognition.py
+++ recognition.py
@@ -2,33 +2,31 @@
 import json
 import base64
 import cv2
+import numpy as np
 
 class Recognition:
     """Recognition"""
 
-        def __init__(self,params: dict) -> result: dict:
+    def __init__(self,params: dict):
 
-            self.request_number = params['request_number']
-            self.id = params['id']
-            self.input_image = params['input_image']
-            self.action = params['action']
-
-    
-        def convert_base64_to_img(img_b64):
-
-            if ',' in img_b64:
-                img_b64 = self['input_image'].split(',')[-1]
-                
-            img_b64 = base64.b64decode(img_b64)
-            img_b64 = np.frombuffer(img_b64,dtype = np.uint8)
-            img = cv2.imdecode(img_b64,flags=1)
-
-            return img    
+        self.request_number = params['request_number']
+        self.id = params['id']
+        self.img_b64 = params['img_base64']
+        self.action = params['action']
 
 
-        def convert_img_to_base64(img):
+    def convert_base64_to_img(self):
+        
+        img_b64 = self.img_b64
+        #if ',' in img_b64:
+        #    img_b64 = img_b64.split(',')[-1]
+            
+        img_b64 = base64.b64decode(img_b64)
+        img_b64 = np.frombuffer(img_b64,dtype = np.uint8)
+        self.img = cv2.imdecode(img_b64,flags=1)
 
-            _, img_b64(cv2.imencode('.jpg',img)
 
-            return base64.b64encode(img_b64)
+    def convert_img_to_base64(self):
 
+        _, img_b64 = cv2.imencode('.jpg',self.input_image)
+        self.img = base64.b64encode(img_b64)
